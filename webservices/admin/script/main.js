a2935d134b8c3c697f5fb7d96bcea4b9dd52afaa
lchaconc <luis.chacon.campos@gmail.com>
2020-06-09 10:44:47 -0600

    fin fase de validacion de input

--- webservices/admin/script/main.js
+++ webservices/admin/script/main.js
@@ -1,43 +1,75 @@
 $(document).ready(function () {
     $("#btnEnviar").click(function (e) { 
         e.preventDefault();
-        enviar();
+        handleEnviar();
+       
     });
 });
 
-function enviar() {
-    var formData = new FormData();
-    formData.append("gnabere", $("#gnabere").val());
-    formData.append("espanol", $("#espanol").val());
-    formData.append("txtGnabere", $("#txtGnabere").val());
-    formData.append("txtEspanol", $("#txtEspanol").val());
-    formData.append("audio", $('#audio')[0].files[0]);
-    formData.append("imagen", $('#imagen')[0].files[0]);
-  console.log("Tratando de enviar",$('#imagen')[0].files[0]);
-  console.log(formData);
-   
-    $.ajax({      
-      url: '../enviar_termino.php',
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      cache: false,
-      contentType: false,
-      processData: false,
-      beforeSend: function(){
-      console.log("En proceso");
-      $(".div-shadow").removeClass("invisible");
-      }, success: function(response){
-        console.log("Enviado");
-        $(".div-shadow").addClass("invisible");
+
+function validarArchivos( ) {
+  let lleno=false
+  if ( typeof ($('#audio')[0].files[0]) == 'object' &&  typeof($('#imagen')[0].files[0] ) == 'object'  ) {
+    lleno=true
+  } 
+  return lleno
+}
+
+function validarTextos () {
+  let lleno=false
+  if ($("#gnabere").val() &&  $("#espanol").val() &&   $("#txtGnabere").val() && $("#txtEspanol").val() ) {
+    lleno=true    
+  } 
+  return lleno
+}
+
+function ajaxServer() {
+  var formData = new FormData();
+  formData.append("gnabere", $("#gnabere").val() );
+  formData.append("espanol", $("#espanol").val() );
+  formData.append("txtGnabere", $("#txtGnabere").val() );
+  formData.append("txtEspanol", $("#txtEspanol").val() );
+  formData.append("audio", $('#audio')[0].files[0] );
+  formData.append("imagen", $('#imagen')[0].files[0] );
+
+    //console.log("Tratando de enviar",$('#imagen')[0].files[0]);
+    console.log("formData", formData);
+
+  $.ajax({      
+    url: '../enviar_termino.php',
+    type: 'POST',
+    data: formData,
+    dataType: 'json',
+    cache: false,
+    contentType: false,
+    processData: false,
+    beforeSend: function(){
+    console.log("En proceso");
+    $(".div-shadow").removeClass("invisible");
+    }, success: function(response){
+      console.log("Enviado");
+      $(".div-shadow").addClass("invisible");
+      console.log(response.msj);
+      // function (response);
+
+    }, error: function(response){
+      console.log("Error al enviar");
+        console.log(response.error);
         console.log(response.msj);
-        // function (response);
+        $(".div-shadow").addClass("invisible");
+    }
+  });
+}
+
+function handleEnviar() {
+    console.log(validarTextos() )
+    if (validarTextos() && validarArchivos()  ) {
+      ajaxServer()
+    } else {
+      alert ("Debe completar todos los campos")
+    }
+    
+
   
-      }, error: function(response){
-        console.log("Error al enviar");
-          console.log(response.error);
-          console.log(response.msj);
-          $(".div-shadow").addClass("invisible");
-      }
-    }); 
+  
 }
