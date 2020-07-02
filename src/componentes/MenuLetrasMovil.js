bf2e1665a7b52dae692aa5761b6a49c5e4519fd2
arkoscr <tutordejava@gmail.com>
2020-07-01 20:09:24 -0600

    Ajustes a versión móvil (scroll menu)

--- src/componentes/MenuLetrasMovil.js
+++ src/componentes/MenuLetrasMovil.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import palabras from '../data/palabras.json';
 
 function MenuLetrasMovil(props) {
-return(
-    <React.Fragment>  
-    <div className="row text-center">
-        {/* <div className="col-3">
-            <img className="esquinaMovil" src="https://recursos.mep.go.cr/2020/oscar/traductor/webservices/borde_izq-movil.png" alt="esquina"/>
-        </div>  */}
-        <div className="col-12 letrasMovil">
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 6
+        }
+      }; 
+    return (
+        <React.Fragment> 
+            <div className="col-1"></div>
+            <div className="col-10 letrasMovil">
+            <Carousel 
+                responsive={responsive}  
+                containerClass="carousel-container"
+                // removeArrowOnDeviceType={["tablet", "mobile"]}
+                itemClass="carousel-item-padding-40-px"
+                >
             {
                 palabras.map((item,i)=>(
                 <button className="parMovil img-fluid" onClick={props.handleObtenerPorLetra} key={i} id={item.letra} >{item.letra}</button>
                 ))
             }
             
-        </div>
-    </div>
-    </React.Fragment>
-)
-    }
-    export default MenuLetrasMovil;
+            </Carousel>
+            </div>
+            <div className="col-1"></div>
+        </React.Fragment>
+      );
+  }
+  export default  MenuLetrasMovil;
